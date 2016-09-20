6ed61c953adf58332c4324bddc8bb5713629f7c5
Sayem <afm.sayem@gmail.com>
2016-09-20 22:50:39 +0600

    Set/Retrieve data from movie store

--- src/services/movie.js
+++ src/services/movie.js
@@ -1,15 +1,30 @@
 import {inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-fetch-client';
 import {format} from 'objection-find-query';
+import {buildQueryString} from 'aurelia-path';
+import {EventAggregator} from 'aurelia-event-aggregator';
 
-@inject(HttpClient)
+@inject(HttpClient, EventAggregator)
 export class MovieApi {
-  constructor(http) {
+  constructor(http, ea) {
     this.http = http;
+    this.ea = ea;
   }
 
-  getMovies(options) {
-    return this.http.fetch('movies');
+  setMovies(options) {
+    return this.http.fetch('movies')
+      .then(response => response.json())
+      .then(data => {
+        this._movies = data;
+        this.ea.publish('movie:index');
+      })
+      .catch(error => {
+        this._error = error;
+      });
+  }
+
+  get movies() {
+    return this._movies;
   }
 
 }
--- src/views/movie/movie.js
+++ src/views/movie/movie.js
@@ -1,18 +1,18 @@
 import {inject} from 'aurelia-framework';
 import {MovieApi} from '../../services/movie';
+import {EventAggregator} from 'aurelia-event-aggregator';
 
-@inject(MovieApi)
+@inject(MovieApi, EventAggregator)
 export class Movie {
-  constructor(movieApi) {
+  constructor(movieApi, ea) {
     this.movieApi = movieApi;
+    this.ea = ea;
   }
 
   activate() {
-    this.movieApi
-      .getMovies()
-      .then(res => res.json())
-      .then(movies => {
-        this.movies = movies.results;
-      });
+    this.movieApi.setMovies();
+    this.ea.subscribe('movie:index', () => {
+      this.movies = this.movieApi.movies.results;
+    });
   }
 }
