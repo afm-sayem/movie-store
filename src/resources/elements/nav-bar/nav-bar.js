be3093aa965704fe006e2ca8b07cd371a1596173
Sayem <afm.sayem@gmail.com>
2016-09-22 21:51:14 +0600

    Update bourbon

--- src/resources/elements/nav-bar/nav-bar.js
+++ src/resources/elements/nav-bar/nav-bar.js
@@ -1,24 +1,12 @@
 import {customElement, bindable, containerless} from 'aurelia-framework';
-import {CssAnimator} from 'aurelia-animator-css';
-import {inject} from 'aurelia-framework';
+// import {CssAnimator} from 'aurelia-animator-css';
+// import {inject} from 'aurelia-framework';
 
-@customElement('nav-bar')
-@containerless()
-@inject(CssAnimator)
+// @customElement('nav-bar')
+// @inject(CssAnimator)
 export class NavBar {
   @bindable router;
-  constructor(animator) {
-    this.animator = animator;
-  }
-
-  menuClick() {
-    // slide in if it's in view, else slide out
-    if (this.menu.classList.contains('show')) {
-      this.animator.removeClass(this.menu, 'show');
-      this.animator.addClass(this.menu, 'hide');
-    } else if (this.menu.classList.contains('hide')) {
-      this.animator.removeClass(this.menu, 'hide');
-      this.animator.addClass(this.menu, 'show');
-    }
-  }
+  // constructor(animator) {
+  //   this.animator = animator;
+  // }
 }
